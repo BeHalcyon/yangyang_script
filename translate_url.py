42bcc3e9a23807168aca9de6d24fd83221d28a28
xiangyanghe <xiangyanghe@zju.edu.cn>
2022-03-16 13:27:24 +0800

    extract redpacket 100

--- translate_url.py
+++ translate_url.py
@@ -12,8 +12,10 @@ def url(header, body):
         body = body.replace(k, v)
     return header + "&body=" + body
 
-header = 'https://api.m.jd.com/client.action?functionId=newBabelAwardCollection'
-body = '{"couponSource":"ace","couponSourceDetail":null,"eid":"eidAc18e812136s4nTjtooubT3ilXFcHm4cYWp047yFwDgQUNBDtBBL8hAcO30AD++373zBE1iv9iNKLCWclDhywo3299Kkm3mr+x0dOrvzHVD9ZmZ5W","extend":"0271DFD6890D3B60ACB8BA8A9E49BEB17FE8E6323A36834B63FE69E95D38088E4F2382B6F3D785A866EE750B76429E5132C90E76D8AB5619327E01D1578C6F08B6BD9E05DFCD8856878CFD0DFB662DCFEEAD7C70516C06C40B020D94A5452E71","lat":"","lng":"","pageClickKey":"Coupons_GetCenter","rcType":"1","shshshfpb":"JD012145b9SEqmv1RLhi164457505239906mrUOCZoxkzRWlNiRb5lmvrBnT2Vzl3RnVjIKZTlp_dX4pEZkKez6uZ2rhOv5qouTW6OWG_OPAirzGe5Ih1s8GSUA8huiifnWgYGkePiJjEg0ztgann~k0ShE0VFfS0uLrm3yoNumA5uD3r3emQ2FL8eLhl_nVQm9h1UL6G1c50WKI9cK9CGrc72sGp6QaA_KJe-2T4vNjsZC85hKH-dijSGLBXgj-_Zrzk5zXO1VYUYqsykIGFW2XQo-It8KicT93wlvPuOc3-zweMofPunqpTzJekgy6Bc","source":"couponCenter_app"}'
+header = 'https://api.m.jd.com/client.action?functionId=withdrawRedPocket&client=wh5&clientVersion=1.0.0&uuid=865441035434968-4c49e3f54533'
+header = 'https://api.m.jd.com/client.action?functionId=withdrawRedPocket&client=wh5&clientVersion=1.0.0&uuid=865441035434968-4c49e3f54533'
+header = 'https://api.m.jd.com/client.action?functionId=withdrawRedPocket&client=wh5&clientVersion=1.0.0&uuid=865441035434968-4c49e3f54533'
+body = '{"depositeType":1}'
 
 print(url(header, body))
 
