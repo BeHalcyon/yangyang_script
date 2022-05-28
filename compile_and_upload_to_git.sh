08e8c24e8c770122e80910184840765f18ef99da
xiangyanghe <xiangyanghe@zju.edu.cn>
2022-05-28 23:06:20 +0800

    .

--- compile_and_upload_to_git.sh
+++ compile_and_upload_to_git.sh
@@ -1,7 +1,14 @@
 
+
+cd /root/project/yangyang_script
+
 python3 setup.py build_ext --inplace
-mv exchange_lib.cpython-36m-x86_64-linux-gnu.so exchange_lib.so
+mv ./exchange_lib.cpython-36m-x86_64-linux-gnu.so ./exchange_lib.so
+
+cd /root/project/yangyang_script
 
 git add -A
 git commit -m "."
 git push origin master
+
+rm exchange_lib.so
