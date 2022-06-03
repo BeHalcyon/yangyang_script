705bbaa50eca0d7b2069bdc4bc1f8b860a6ff9c5
xiangyanghe <xiangyanghe@zju.edu.cn>
2022-06-04 01:42:19 +0800

    .

--- coupons_for_desktop/exchange_59_20_desktop.py
+++ coupons_for_desktop/exchange_59_20_desktop.py
@@ -336,7 +336,6 @@ def getSignAPIWithEncrypt(functionId, body, user_agent, pin):
         exit()
 
 
-# 产生随机子串
 def randomString(e, flag=False):
     t = "0123456789abcdef"
     if flag: t = t.upper()
@@ -346,7 +345,7 @@ def randomString(e, flag=False):
 
 # 59-20券的receiveKey
 def getCcFeedInfo(cookie, receive_dict):
-    re_body = re.compile(r'body=.*?&')
+    # re_body = re.compile(r'body=.*?&')
     body = {
         "categoryId": 118,
         "childActivityUrl": "openapp.jdmobile://virtual?params={\"category\":\"jump\",\"des\":\"couponCenter\"}",
@@ -363,27 +362,36 @@ def getCcFeedInfo(cookie, receive_dict):
         "shshshfpb": ""
     }
 
+    # res = getSignAPIWithEncrypt('getCcFeedInfo', body, "", "")
     res = getSignAPIWithEncrypt('getCcFeedInfo', body, "", "")
 
     if res == -1:
         return -1
     else:
         result = res['body']
-        functionId = result['functionId']
 
-        rsbody = json.loads(json.dumps(result['body']))
-        url = f'https://api.m.jd.com?functionId={functionId}&body=' + rsbody
+        functionId = result['functionId']
+        del result['functionId']
+
+        # if 'ep_encrypt_uuid' in result:
+        #     del result['ep_encrypt_uuid']
+
+        # rsbody = json.loads(json.dumps(result['body']))
+        # url = f'https://api.m.jd.com?functionId={functionId}&body=' + rsbody
+        url = f'https://api.m.jd.com?functionId={functionId}'
 
         headers = {
             "Host": "api.m.jd.com",
             "cookie": cookie,
             "charset": "UTF-8",
             "user-agent": user_agent,
+            # "user-agent": "okhttp/3.12.1;jdmall;android;version/10.1.4;build/90060;screen/720x1464;os/7.1.2;network/wifi;",
             "accept-encoding": "br,gzip,deflate",
             "cache-control": "no-cache",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "content-length": str(len(rsbody)),
+            "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+            "content-length": str(len(result))
         }
+
 
         res = requests.post(url=url, headers=headers, data=result, timeout=30).json()
 
@@ -395,11 +403,12 @@ def getCcFeedInfo(cookie, receive_dict):
                         receive_dict[cookie] = receiveKey
                         return receiveKey
 
-            print('没有找到59-20券的receiveKey')
+            printT(f'User: {getUserName(cookie)}, cannot find the receive key for coupon 59-20!')
             receive_dict[cookie] = ""
             return -1
         else:
-            print('获取59-20券的receiveKey失败')
+            # print('获取59-20券的receiveKey失败')
+            printT(f'User: {getUserName(cookie)}, fail to get the receive key for coupon 59-20!')
             receive_dict[cookie] = ""
             return -1
 
@@ -504,7 +513,7 @@ def receiveNecklaceCouponThread(cookie, api_para, mask_dict, thread_id=0, thread
         printT(prefix_info + target_info)
     except:
         pass
-    if "已经兑换过" in target_info:
+    if "已经兑换过" in target_info or "太贪心" in target_info:
         mask_dict[cookie] = -1
     elif "不足" in target_info:
         mask_dict[cookie] = 0
@@ -536,9 +545,9 @@ def exchangeV4(batch_size=4, waiting_delta=0.26, thread_number=12, sleep_time=0.
 
     # get cookies from databse
     database_dict = {
-            'type': 'sqlite',
-            'name': "filtered_cks.db"
-        }
+        'type': 'sqlite',
+        'name': "filtered_cks.db"
+    }
     # 存入本地数据库
     database = SQLProcess("ck6_59_20_" + time.strftime("%Y%W"), database_dict)
     # 插入所有数据，如果存在则更新
@@ -556,7 +565,6 @@ def exchangeV4(batch_size=4, waiting_delta=0.26, thread_number=12, sleep_time=0.
     cookies, visit_times = database.filterUsers(user_number=batch_size, year_month_day=today_week_str)
     cookies = cookies[:min(len(cookies), batch_size)]
     random.shuffle(cookies)
-
 
     print("\nAccount ready to run：")
     print("\n".join([getUserName(ck) for ck in cookies]), '\n')
@@ -701,9 +709,9 @@ def exchange0Clock(batch_size=4, waiting_delta=0.0, loop_times=1, sleep_time=0.0
     cookies = os.environ["JD_COOKIE"].split('&') if "JD_COOKIE" in os.environ else []
     # get cookies from databse
     database_dict = {
-            'type': 'sqlite',
-            'name': "filtered_cks.db"
-        }
+        'type': 'sqlite',
+        'name': "filtered_cks.db"
+    }
     # 存入数据库
     # Debug
     database = SQLProcess("ck6_59_20_" + (datetime.datetime.now() + datetime.timedelta(hours=1)).strftime("%Y%W"),
@@ -814,44 +822,56 @@ def loopForDays(
     clock_list.sort()
     clock_list.append(clock_list[0])
 
-    while True:
-        # get next start hour
-        cur_hour = int(datetime.datetime.now().strftime("%H"))
-        next_clock = clock_list[0]
-        for c in clock_list:
-            if c > cur_hour:
-                next_clock = c
+    # 本地运行环境
+    if not ('QINGLONG_ENVIRON' in os.environ and os.environ['QINGLONG_ENVIRON'] == "True"):
+        while True:
+            # get next start hour
+            cur_hour = int(datetime.datetime.now().strftime("%H"))
+            next_clock = clock_list[0]
+            for c in clock_list:
+                if c > cur_hour:
+                    next_clock = c
+                    break
+
+            # 修复非整点运行的bug
+            next_task_start_time = datetime.datetime.now().replace(hour=(next_clock + 23) % 24,
+                                                                   minute=59,
+                                                                   second=5,
+                                                                   microsecond=0)
+
+            waiting_time = (next_task_start_time - datetime.datetime.now()).total_seconds()
+
+            if not ('DEBUG_59_20' in os.environ and os.environ['DEBUG_59_20'] == 'True'):
+                print("\n" + ("Waiting to " + str(next_task_start_time)).center(80, "*") + "\n")
+                printT(f"Waiting {waiting_time}s.")
+                time.sleep(waiting_time)
+
+            ts_time = int(round((datetime.datetime.now() - datetime.timedelta(minutes=5)).timestamp() * 1000))
+
+            printT(f"Starting coupon in {next_task_start_time}...")
+
+            # 修复参数异常的bug
+            if next_clock == 0:
+                exchange0Clock(batch_size=6, waiting_delta=second_ahead * 2.3, loop_times=1, sleep_time=sleep_time)
+            else:
+                exchangeV4(batch_size=6, waiting_delta=second_ahead, thread_number=thread_number, sleep_time=sleep_time,
+                           coupon_type=coupon_type)
+
+
+            if 'DEBUG_59_20' in os.environ and os.environ['DEBUG_59_20'] == 'True':
+                print()
+                printT("Test end. Please set os.environ['DEBUG_59_20'] = \"False\"")
                 break
 
-        next_task_start_time = (datetime.datetime.now() + datetime.timedelta(minutes=30)).replace(
-            hour=(next_clock + 23) % 24,
-            minute=59,
-            second=5,
-            microsecond=0)
-
-        waiting_time = (next_task_start_time - datetime.datetime.now()).total_seconds()
-        if not ('DEBUG_59_20' in os.environ and os.environ['DEBUG_59_20'] == 'True'):
-            print("\n" + ("Waiting to " + str(next_task_start_time)).center(80, "*") + "\n")
-            printT(f"Waiting {waiting_time}s.")
-            time.sleep(waiting_time)
-
-        ts_time = int(round((datetime.datetime.now() - datetime.timedelta(minutes=5)).timestamp() * 1000))
-
-        printT(f"Starting coupon in {next_task_start_time}...")
-
-        if next_clock == 0:
-            exchange0Clock(batch_size=6, waiting_delta=second_ahead * 2.3, loop_times=1, sleep_time=sleep_time,
-                           coupon_type=coupon_type)
+            printT("Waiting 20s...")
+            time.sleep(20)
+    # 青龙运行环境
+    else:
+        if datetime.datetime.now().strftime('%H') == '23':
+            exchange0Clock(batch_size=6, waiting_delta=second_ahead * 2.3, loop_times=1, sleep_time=sleep_time)
         else:
-            exchangeV4(batch_size=6, waiting_delta=second_ahead, thread_number=thread_number, sleep_time=sleep_time)
-
-        if 'DEBUG_59_20' in os.environ and os.environ['DEBUG_59_20'] == 'True':
-            printT("\nTest end. Please set os.environ['DEBUG_59_20'] = \"False\"")
-            break
-
-        printT("Waiting 20s...")
-        time.sleep(20)
-
+            exchangeV4(batch_size=6, waiting_delta=second_ahead, thread_number=thread_number, sleep_time=sleep_time,
+                       coupon_type=coupon_type)
 
 user_agent = "okhttp/3.12.1;jdmall;android;version/11.0.2;build/97565;"
 ts_time = int(round((datetime.datetime.now() - datetime.timedelta(minutes=5)).timestamp() * 1000))
@@ -860,20 +880,27 @@ if __name__ == '__main__':
     # TODO
     # Necessary
     # 服务器接口
-    os.environ["JD_SIGN_API"] = ""
-    os.environ["JD_SIGN_API_TOKEN"] = ""
+    os.environ["JD_SIGN_API"] = "http://43.152.219.53:9091/yangyang/jdSignWithEncryptV2"
+    os.environ["JD_SIGN_API_TOKEN"] = "LKHJFPOajslfkjPFoasdfajQOIJ"
 
     # TODO
     # Not necessary: Add wxpusher notification
     # https://wxpusher.zjiecode.com/admin/login
-    os.environ["WXPUSHER_APP_TOKEN"] = ""
-    os.environ["WXPUSHER_UID"] = ""
+    os.environ["WXPUSHER_APP_TOKEN"] = "AT_2us2aVEfFfvcl2zk8E3fWQwyxEgqYt6c"
+    os.environ["WXPUSHER_UID"] = "UID_tsUTi2JU8CZxW7b58OryyaIdaiOW"
 
     # TODO
     # 调试时，将False修改为True，会默认当前时刻运行，注意首字母大写
     # 重要：部署时，将True修改为False，会默认整点运行
-    os.environ['DEBUG_59_20'] = "False"
+    os.environ['DEBUG_59_20'] = "True"
     # os.environ['DEBUG_59_20'] = "False"
+
+    # TODO
+    # 青龙环境下，需要将该参数修改为True，并设置自动定时任务
+    # 本地环境默认为False
+    # 命令： task path/to/file/exchange_59_20_desktop.py
+    # cron: 0 59 9,13,17,21,23 * * *
+    os.environ['QINGLONG_ENVIRON'] = "False"
 
     # TODO
     # 建议3个号起
@@ -881,20 +908,20 @@ if __name__ == '__main__':
     # 示例如下。少号时增加，多号时删除，账号越前面，优先级越高
     # 包括字段pin  wskey  whwswswws  unionwsws
     # 前n-1行尾部有','，最后一行尾部没有
+    # 青龙环境中需要添加环境适配
     os.environ["JD_COOKIE"] = "&".join([
-        'pin=jd_xxxxxxx;wskey=xxxxxxxxxxxxxxxxxxxx;whwswswws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;unionwsws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;',
-        'pin=jd_xxxxxxx;wskey=xxxxxxxxxxxxxxxxxxxx;whwswswws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;unionwsws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;',
-        'pin=jd_xxxxxxx;wskey=xxxxxxxxxxxxxxxxxxxx;whwswswws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;unionwsws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;',
-        'pin=jd_xxxxxxx;wskey=xxxxxxxxxxxxxxxxxxxx;whwswswws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;unionwsws=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;'
+        'pin=bao97288;wskey=AAJigQqHAEA9afGoAwUpjna-wvfpM_5BJqe8aHi80nnW_nFzXY9w1gj_uEuEmNHk3rE8lPhdrXTz7Woiikd8G8qAFDdsBan7;whwswswws=JD012145b9xC7XIjP48f165424595203302zbKsCeuQ-nI05bACms2o_I9S-71FlCRk15ptfci~cEh1-BD-Xsi5t4lb894XNMVQic3J4lVabzyM_mnp2rv8DDc7uf150j8dD8jWjp9iqxbbUAuxj3q34n5Bmy1UaSQ;unionwsws={"devicefinger":"eidAeb0a81223ascfgFYjQdARxaKbanKU27pCH4BWgtFG5NyFcNav1oJoWYS12N8SRh4RUliBCre1jDYkVey2THcYDw9ke\/iIZlkvTidVn7SDoag99Q5","jmafinger":"JD012145b9xC7XIjP48f165424595203302zbKsCeuQ-nI05bACms2o_I9S-71FlCRk15ptfci~cEh1-BD-Xsi5t4lb894XNMVQic3J4lVabzyM_mnp2rv8DDc7uf150j8dD8jWjp9iqxbbUAuxj3q34n5Bmy1UaSQ"};',
+        'pin=jd_MJBZujfViUvs;wskey=AAJijsZiAEDrnygS1fPG5i2Mz44xr5-A0M9HKnY4PffO9B9fUCbvRL4xQkC5pteLcBShY8BRSCUmtnzEcv9qTGIE_We73ZOS;whwswswws=JD012145b9Ypn6G3xbeL165408792413705eL9ANu4wXm2lHdsIusrJb-yUVRdFzyqsLGDy3dT7fzt_tzGoSZq4OdqJiSrzQ06QIcYhleTrEWa9ePnA1ojPDfjwGH3nMwZPaIP_vTou3E012vl31j~t3EdXdDgwho38N6TdpPJy24Ls/HBve79oxzHnynvSj9ts2GXqSkGGBka9R1qZ4GdS/Tv30d/yJ00oSMtjMqzZNP1Y2ogCsiBFFIXvWH6rNPyDthoywuj46EbeUAa9OEa5;unionwsws={"devicefinger":"RI5XQ3T4Z7CNI22OG55IB5LKKZSE25VBTYHILYDH5MQGTXCFSW6ZPTLIIWWGSY6IVAS5QKEVELILIABSSQZNH722HJUVNCCU7WE6TX4S4JTABL5OS7SA","jmafinger":"JD012145b9Ypn6G3xbeL165408792413705eL9ANu4wXm2lHdsIusrJb-yUVRdFzyqsLGDy3dT7fzt_tzGoSZq4OdqJiSrzQ06QIcYhleTrEWa9ePnA1ojPDfjwGH3nMwZPaIP_vTou3E012vl31j~t3EdXdDgwho38N6TdpPJy24Ls\/HBve79oxzHnynvSj9ts2GXqSkGGBka9R1qZ4GdS\/Tv30d\/yJ00oSMtjMqzZNP1Y2ogCsiBFFIXvWH6rNPyDthoywuj46EbeUAa9OEa5"};'
     ])
 
     # TODO
     # Need: change parameters
     # 运行一次后挂机，到点时自动抢
     # 到clock_list的前1分钟时，会计算sign，随后提前second_ahead秒运行，共运行thread_number个线程，每个线程之间的间隔时间为sleep_time秒
+
     loopForDays(
-        second_ahead=0.3,                       # 线程启动的提前秒数；线程的冷冻时间；例如提前0.40s准备启动线程；依赖于设备资源
-        sleep_time=0.03,                        # 每个线程的等待时间；每次post的等待时间；依赖于网络传输速率；少号时需适当调节
+        second_ahead=0.35,                      # 线程启动的提前秒数；线程的冷冻时间；例如提前0.40s准备启动线程；依赖于设备资源
+        sleep_time=0.07,                        # 每个线程的等待时间；每次post的等待时间；依赖于网络传输速率；少号时建议0.08；多号时建议0.03
         thread_number=12,                       # 线程数量；post次数；可根据thread_number*sleep_time粗略计算当场运行时间；建议不超过15
         coupon_type="59-20",                    # 券名（可自定义），用于wxpusher通知
-        clock_list=[0, 10, 14, 18, 21, 22])     # 定时任务；例如0点场、10点场...
+        clock_list=[0, 10, 14, 18, 22])         # 定时任务；例如0点场、10点场...
